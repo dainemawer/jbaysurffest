bb69657157f5743c383800d75dffbc209a129551
Daine Mawer <hello@dainemawer.com>
2023-05-29 14:18:00 +0200

    add dropdown to header

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -11,11 +11,53 @@ export const navigation = [
 	},
 	{
 		id: 2,
+		title: 'Activities',
+		link: 'activities',
+		submenu: [
+			{
+				id: 0,
+				title: 'Mountain Biking',
+				link: '/activities/mountain-biking'
+			},
+			{
+				id: 1,
+				title: 'Jui Jitsu',
+				link: '/activities/jui-jitsu'
+			},
+			{
+				id: 2,
+				title: 'Funduro',
+				link: '/activities/funduro'
+			},
+			{
+				id: 3,
+				title: 'Fishing',
+				link: '/activities/fishing'
+			},
+			{
+				id: 4,
+				title: 'Raffle',
+				link: '/activities/raffle'
+			},
+			{
+				id: 5,
+				title: 'Fundraiser',
+				link: '/activities/vip-fundraiser'
+			},
+			{
+				id: 6,
+				title: 'Foundation',
+				link: '/activities/foundation'
+			}
+		]
+	},
+	{
+		id: 3,
 		title: 'Tickets',
 		link: 'tickets'
 	},
 	{
-		id: 3,
+		id: 4,
 		title: 'FAQ',
 		link: 'faq'
 	}
